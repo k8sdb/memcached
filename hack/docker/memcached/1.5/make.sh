3c45185c845fc7aac83898b3fa282a0ba17eebc0
Tamal Saha <tamal@appscode.com>
2018-01-18 21:11:11 -0800

    Update make.sh

--- hack/docker/memcached/1.5/make.sh
+++ hack/docker/memcached/1.5/make.sh
@@ -4,11 +4,12 @@ set -xeou pipefail
 DOCKER_REGISTRY=${DOCKER_REGISTRY:-kubedb}
 IMG=memcached
 TAG=1.5
+PATCH=1.5.4
 
-docker pull $IMG:$TAG-alpine
+docker pull $IMG:$PATCH
 
-docker tag $IMG:$TAG-alpine "$DOCKER_REGISTRY/$IMG:$TAG"
+docker tag "$DOCKER_REGISTRY/$IMG:$PATCH" "$DOCKER_REGISTRY/$IMG:$TAG"
 docker push "$DOCKER_REGISTRY/$IMG:$TAG"
 
-docker tag $IMG:$TAG-alpine "$DOCKER_REGISTRY/$IMG:$TAG-alpine"
+docker tag "$DOCKER_REGISTRY/$IMG:$TAG" "$DOCKER_REGISTRY/$IMG:$TAG-alpine"
 docker push "$DOCKER_REGISTRY/$IMG:$TAG-alpine"

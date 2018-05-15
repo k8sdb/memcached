f69b6e9e9bd32ebb3306088f60a2c024bd59179e
the-redback <maruf@appscode.com>
2018-05-15 12:07:39 +0600

    added-all

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -99,9 +99,10 @@ var _ = BeforeSuite(func() {
 var _ = AfterSuite(func() {
 
 	By("Cleanup Left Overs")
-
-	By("Delete Admission Controller Configs")
-	root.CleanAdmissionConfigs()
+	if !framework.SelfHostedOperator {
+		By("Delete Admission Controller Configs")
+		root.CleanAdmissionConfigs()
+	}
 	By("Delete left over Memcached objects")
 	root.CleanMemcached()
 	By("Delete left over Dormant Database objects")

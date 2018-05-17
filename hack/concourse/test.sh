36c2b196757ac27a8089f2f20ff075c1d712bb63
tahsinrahman <tahsynx@gmail.com>
2018-05-17 20:09:07 +0600

    update pipeline; build and push docker image before creating cluster; update builddeps.sh

--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -7,21 +7,21 @@ entrypoint.sh
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker run hello-world
 
-#install python pip
+# install python pip
 apt-get update > /dev/null
 apt-get install -y python python-pip > /dev/null
 
-#install kubectl
+# install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &> /dev/null
 chmod +x ./kubectl
 mv ./kubectl /bin/kubectl
 
-#install onessl
+# install onessl
 curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64 \
   && chmod +x onessl \
   && mv onessl /usr/local/bin/
 
-#install pharmer
+# install pharmer
 pushd /tmp
 curl -LO https://cdn.appscode.com/binaries/pharmer/0.1.0-rc.3/pharmer-linux-amd64
 chmod +x pharmer-linux-amd64
@@ -44,22 +44,32 @@ function cleanup {
 }
 trap cleanup EXIT
 
+# copy memcached to $GOPATH
+mkdir -p $GOPATH/src/github.com/kubedb
+cp -r memcached $GOPATH/src/github.com/kubedb
+
+pushd $GOPATH/src/github.com/kubedb/memcached
 # name of the cluster
 # nameing is based on repo+commit_hash
-pushd memcached
 NAME=memcached-$(git rev-parse --short HEAD)
+
+./hack/builddeps.sh
+export APPSCODE_ENV=dev
+export DOCKER_REGISTRY=kubedbci
+./hack/docker/mc-operator/make.sh build
+./hack/docker/mc-operator/make.sh push
 popd
 
-#create credential file for pharmer
+# create credential file for pharmer
 cat > cred.json <<EOF
 {
         "token" : "$TOKEN"
 }
 EOF
 
-#create cluster using pharmer
-#note: make sure the zone supports volumes, not all regions support that
-#"We're sorry! Volumes are not available for Droplets on legacy hardware in the NYC3 region"
+# create cluster using pharmer
+# note: make sure the zone supports volumes, not all regions support that
+# "We're sorry! Volumes are not available for Droplets on legacy hardware in the NYC3 region"
 pharmer create credential --from-file=cred.json --provider=DigitalOcean cred
 pharmer create cluster $NAME --provider=digitalocean --zone=nyc1 --nodes=2gb=1 --credential-uid=cred --kubernetes-version=v1.10.0
 pharmer apply $NAME
@@ -68,16 +78,8 @@ pharmer use cluster $NAME
 sleep 120
 kubectl get nodes
 
-#copy memcached to $GOPATH
-mkdir -p $GOPATH/src/github.com/kubedb
-cp -r memcached $GOPATH/src/github.com/kubedb
 pushd $GOPATH/src/github.com/kubedb/memcached
 
 # run tests
-./hack/builddeps.sh
-export APPSCODE_ENV=dev
-export DOCKER_REGISTRY=kubedbci
-./hack/docker/mc-operator/make.sh build
-./hack/docker/mc-operator/make.sh push
 source ./hack/deploy/setup.sh --docker-registry=kubedbci
 ./hack/make.py test e2e --v=1 --selfhosted-operator=true

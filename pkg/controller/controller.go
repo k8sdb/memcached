28cdfdfd18dee78ac16fb7078ff633590d370269
Tamal Saha <tamal@appscode.com>
2020-12-29 14:51:17 -0800

    Initialize mapper
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -36,8 +36,10 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/discovery/cached/memory"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
 	reg_util "kmodules.xyz/client-go/admissionregistration/v1beta1"
@@ -83,6 +85,7 @@ func New(
 			CRDClient:        crdClient,
 			AppCatalogClient: appCatalogClient,
 			ClusterTopology:  topology,
+			Mapper:           restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(client.Discovery())),
 			Recorder:         recorder,
 		},
 		Config:     opt,
